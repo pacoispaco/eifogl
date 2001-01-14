4b221f42d16114491eea1f25ef8fc3f364b300ed
paco <paco>
2001-01-14 15:20:19 +0000

    *** empty log message ***

---
+++ doc/html/index.php
@@ -0,0 +1,26 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+   <meta name="Author" content="Paul Cohen">
+   <meta name="GENERATOR" content="Mozilla/4.7 [en] (WinNT; I) [Netscape]">
+   <title>EiffelOpenGL</title>
+</head>
+<body>
+
+<h1>
+EiffelOpenGL</h1>
+Welcome to the EiffelOpenGL homepage!
+<p>EiffelOpenGL is an open-source project that aims to develop a complete
+Eiffel binding for OpenGL. This binding is implemented as an Eiffel wrapping
+of the OpenGL C API.<b><font size=+2></font></b>
+<p><b><font size=+2><a href="faq.html">FAQ</a></font></b>
+<br><b><font size=+2><a href="install.html">Install</a></font></b>
+<br><font size=-2></font>&nbsp;
+<br><font size=-2></font>&nbsp;<font size=-2></font>
+<p><font size=-2>Author: Paul Cohen</font>
+<br><font size=-2>date: "$Date: 2001/01/14 15:20:19 $"</font>
+<br><font size=-2>revision: "$Revision: 1.1 $"</font>
+<br>&nbsp;
+</body>
+</html>
