9832b1be62476ff84b06f1bc8ddbb5a0a40a425d
paco <paco>
2001-01-14 15:32:41 +0000

    Added link to EiffelOpenGL SourceForge developer homepage

--- doc/html/index.php
+++ doc/html/index.php
@@ -13,14 +13,14 @@ EiffelOpenGL</h1>
 Welcome to the EiffelOpenGL homepage!
 <p>EiffelOpenGL is an open-source project that aims to develop a complete
 Eiffel binding for OpenGL. This binding is implemented as an Eiffel wrapping
-of the OpenGL C API.<b><font size=+2></font></b>
+of the OpenGL C API.
 <p><b><font size=+2><a href="faq.html">FAQ</a></font></b>
 <br><b><font size=+2><a href="install.html">Install</a></font></b>
-<br><font size=-2></font>&nbsp;
-<br><font size=-2></font>&nbsp;<font size=-2></font>
+<p><b><font size=+2><a href="https://sourceforge.net/projects/eifogl/">EiffelOpenGL
+SourceForge Developer HomePage</a></font></b>
 <p><font size=-2>Author: Paul Cohen</font>
-<br><font size=-2>date: "$Date: 2001/01/14 15:20:19 $"</font>
-<br><font size=-2>revision: "$Revision: 1.1 $"</font>
+<br><font size=-2>date: "$Date: 2001/01/14 15:32:41 $"</font>
+<br><font size=-2>revision: "$Revision: 1.2 $"</font>
 <br>&nbsp;
 </body>
 </html>
