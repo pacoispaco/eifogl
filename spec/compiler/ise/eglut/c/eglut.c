9000bd51c05ce93833de6d0d282711e5d7b8f6dd
paco <paco>
2002-11-25 16:47:33 +0000

    First version

---
+++ spec/compiler/ise/eglut/c/eglut.c
@@ -0,0 +1,95 @@
+/* 
+ * EGLUT.C
+ *
+ * Eiffel GLUT callback functions
+ *
+ * desciption: "The Eiffel GLUT encapsulation needs to define 
+ *              the callback functions that will be registered 
+ *              with GLUT for each GLUT window. Note that the 
+ *              same event callback function is used for every 
+ *              window! It is simply registered for the window 
+ *              that currently is the current GLUT window!" 
+ * library: "EGLUT - Eiffel wrapping of the OpenGL GLUT library"
+ * author: "Paul Cohen"
+ * copyright: "Copyright (c) 1999 Paul Cohen, see file forum.txt"
+ * date: "$Date: 2002/11/25 16:47:33 $"
+ * version: "$Revision: 1.1 $"
+ */
+
+#ifndef __EGLUT__
+#include "eglut.h"
+#endif
+
+EIF_OBJECT eglut_app = NULL;
+/* Address of the Eiffel object EGLUT_APP created for each application. */
+EIF_PROCEDURE eglut_ep;
+/* Peference to the main_application_callback feature. */
+
+void eglut_initialize_library (EIF_OBJ an_eglut_app) {
+  EIF_TYPE_ID eti;
+
+  eglut_app = an_eglut_app;
+  /* Since the same feature is called by all callbacks (and the code does 
+     not move) we should be able to do this. */
+  eti = eif_type_id ("EGLUT_APPLICATION");
+
+  if (eti == EIF_NO_TYPE) {
+	 eif_panic ("Not a type");
+  }
+
+  eglut_ep = eif_procedure ("main_application_callback", eti);
+
+  if (eglut_ep == NULL) {
+	 eif_panic ("eglut_ep == NULL!");
+  }
+}
+  
+void eglut_display_function (void) {
+  /* Display callback function. */
+  (eglut_ep) (eif_access (eglut_app), EGLUT_DISPLAY_EVENT, 0, 0, 0, 0, '/0');
+}
+
+void eglut_reshape_function (int w, int h) {
+  /* Reshape callback function. */
+  (eglut_ep) (eif_access (eglut_app), EGLUT_RESHAPE_EVENT, w, h, 0, 0, '/0');
+}
+
+void eglut_keyboard_function (unsigned char key, int x, int y) {
+  /* Keyboard callback function. */
+  (eglut_ep) (eif_access (eglut_app), EGLUT_KEYBOARD_EVENT, x, y, 0, 0, key);
+}
+
+void eglut_motion_function (int x, int y) {
+  /* Motion callback function. */
+  (eglut_ep) (eif_access (eglut_app), EGLUT_MOTION_EVENT, x, y, 0, 0, '/0');
+}
+
+void eglut_passive_motion_function (int x, int y) {
+  /* PAssive motion callback function. */
+  (eglut_ep) (eif_access (eglut_app), EGLUT_PASSIVE_MOTION_EVENT, x, y, 0, 0, '/0');
+}
+
+void eglut_mouse_function (int button, int state, int x, int y) {
+  /* Mouse callback function. */
+  (eglut_ep) (eif_access (eglut_app), EGLUT_MOUSE_EVENT, button, state, x, y, '/0');
+}
+
+void eglut_visibility_function (int state) {
+  /* PAssive motion callback function. */
+  (eglut_ep) (eif_access (eglut_app), EGLUT_VISIBILITY_EVENT, state, 0, 0, 0, '/0');
+}
+
+void eglut_entry_function (int state) {
+  /* PAssive motion callback function. */
+  (eglut_ep) (eif_access (eglut_app), EGLUT_ENTRY_EVENT, state, 0, 0, 0, '/0');
+}
+
+void eglut_special_function (int key, int x, int y) {
+  /* Passive motion callback function. */
+  (eglut_ep) (eif_access (eglut_app), EGLUT_SPECIAL_EVENT, key, x, y, 0, '/0');
+}
+
+void eglut_idle_function (void) {
+  /* Idle callback function. */
+  (eglut_ep) (eif_access (eglut_app), EGLUT_IDLE_EVENT, 0, 0, 0, 0, '/0');
+}
---
+++ spec/compiler/ise/eglut/include/eglut.h
@@ -0,0 +1,79 @@
+/* 
+ * EGLUT.H
+ *
+ * desciption: "Header file for EGLUT.C"
+ * library: "EGLUT - Eiffel wrapping of the OpenGL GLUT library"
+ * author: "Paul Cohen"
+ * copyright: "Copyright (c) 1999, 2002 Paul Cohen, see file forum.txt"
+ * date: "$Date: 2002/11/25 16:48:41 $"
+ * revision: "$Revision: 1.1 $"
+ */
+
+#ifndef __EGLUT__
+#define __EGLUT__
+
+#ifndef _eif_eiffel_h_
+#	include <eif_eiffel.h>
+#endif
+
+#ifndef __glut_h__
+#include <glut.h>
+#endif
+
+/* Callback event constants */
+#define EGLUT_DISPLAY_EVENT 1
+#define EGLUT_RESHAPE_EVENT 2
+#define EGLUT_KEYBOARD_EVENT 3
+#define EGLUT_MOUSE_EVENT 4
+#define EGLUT_MOTION_EVENT 5
+#define EGLUT_PASSIVE_MOTION_EVENT 6
+#define EGLUT_VISIBILITY_EVENT 7
+#define EGLUT_ENTRY_EVENT 8
+#define EGLUT_SPECIAL_EVENT 9
+#define EGLUT_IDLE_EVENT 10
+
+/* Set macros for the callback functions */
+/* glutDisplayFunc(NULL) is illegal! */
+#define eglut_set_display_func glutDisplayFunc(eglut_display_function)
+#define eglut_set_reshape_func_to_null glutReshapeFunc(NULL)
+#define eglut_set_reshape_func glutReshapeFunc(eglut_reshape_function)
+#define eglut_set_keyboard_func_to_null glutKeyboardFunc(NULL)
+#define eglut_set_keyboard_func glutKeyboardFunc(eglut_keyboard_function)
+#define eglut_set_motion_func_to_null glutMotionFunc(NULL)
+#define eglut_set_motion_func glutMotionFunc(eglut_motion_function)
+#define eglut_set_passive_motion_func_to_null glutPassiveMotionFunc(NULL)
+#define eglut_set_passive_motion_func glutPassiveMotionFunc(eglut_passive_motion_function)
+#define eglut_set_mouse_func_to_null glutMouseFunc(NULL)
+#define eglut_set_mouse_func glutMouseFunc(eglut_mouse_function)
+#define eglut_set_visibility_func_to_null glutVisibilityFunc(NULL)
+#define eglut_set_visibility_func glutVisibilityFunc(eglut_visibility_function)
+#define eglut_set_entry_func_to_null glutEntryFunc(NULL)
+#define eglut_set_entry_func glutEntryFunc(eglut_entry_function)
+#define eglut_set_special_func_to_null glutSpecialFunc(NULL)
+#define eglut_set_special_func glutSpecialFunc(eglut_special_function)
+#define eglut_set_idle_func_to_null glutIdleFunc(NULL)
+#define eglut_set_idle_func glutIdleFunc(eglut_idle_function)
+
+/* Set macro for the EGLUT_APPLICATION object */
+#define eglut_set_application_object(_addr_) (eglut_app=(EIF_OBJ)_addr_)
+
+/* Macro for quitting the application */
+#define eglut_quit(_exitcode_) exit(_exitcode_)
+
+extern EIF_OBJ eglut_app;
+/* Address of the Eiffel object EGLUT_APPLICATION created for each application. */
+
+/* EGLUT global function prototypes */
+void eglut_display_function (void);
+void eglut_reshape_function (int, int);
+void eglut_keyboard_function (unsigned char, int, int);
+void eglut_motion_function (int, int);
+void eglut_passive_motion_function (int, int);
+void eglut_mouse_function (int, int, int, int);
+void eglut_visibility_function (int);
+void eglut_entry_function (int);
+void eglut_special_function (int, int, int);
+void eglut_idle_function (void);
+
+#endif
+
