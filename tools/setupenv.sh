d5d26cf1ff956158467eed35aaad0773d1526145
Paul Cohen <pacoispaco@gmail.com>
2016-08-13 11:23:48 +0200

    Add setupenv.sh to tools

---
+++ tools/setupenv.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Script for setting up environment variables for Eiffel an EiffelOpenGL
+# Copyright (c) 2016 Paul Cohen
+
+# Set these environment variables as appropriate for your development enviroment
+export ISE_EIFFEL=/usr/local/Eiffel_16.05
+export EIFOGL=/home/paco/proj/eifogl
